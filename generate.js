b9ee95044fd26da36de5c0fea735c2c8c646621c
u-pos <fresta522@yahoo.co.jp>
2026-04-21 18:12:24 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -35,7 +35,7 @@ async function pickImportantNews(titles) {
   }
 
 const res = await fetch(
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + process.env.GEMINI_API_KEY,
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
